b20adb57a8cb3f70360d6891af7109f3d1c9c655
Nura2005 <157690866+Nura2005@users.noreply.github.com>
2024-06-10 13:35:23 +0600

    Add files via upload

---
+++ BEECROWD/BEEC1003.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class BEEC1003 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+
+        int SOMA = A + B;
+
+        System.out.println("SOMA = " + SOMA);
+
+        scanner.close();
+    }
+}
